d324146994fb81d3072219b68858db99c0c1fe80
superco <habibyafi45@gmail.com>
2022-04-11 05:09:31 +0700

    add product constant

---
+++ src/main/java/constants/ProductConstant.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class ProductConstant {
+
+    public static String NAME = "Name";
+    public static String DESCRIPTION = "Description";
+    public static String IMAGE_LINK = "Image Link";
+    public static String PRICE = "Price";
+    public static String RATING = "Rating";
+    public static String MERCHANT = "Merchant";
+}
